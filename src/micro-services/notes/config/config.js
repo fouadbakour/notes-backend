c5f9db57b49744e1ec9beed969dab40947183ccb
Fouad Bakkour <fouad.b@euriskomobility.com>
2022-06-04 00:40:40 +0300

    fix README

--- src/micro-services/notes/config/config.js
+++ src/micro-services/notes/config/config.js
@@ -1,4 +1,4 @@
-const MONGO_URI = process.env.MONGO_URL || 'mongodb+srv://dbusername:pass1234@cluster0.r26guxu.mongodb.net/notes?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MONGO_URL || 'mongodb://localhost:27017/notes-app';
 const PORT = process.env.PORT || 3000;
 const IP = process.env.HOST || '0.0.0.0';
 const JWT_SECRET = process.env.JWT_SECRET || 'jkl!±@£!@ghj1237';
