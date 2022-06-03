2f5361faa7f68dd4c45b67e96225fef668471655
Fouad Bakkour <fouad.b@euriskomobility.com>
2022-06-04 00:27:39 +0300

    WIP

--- src/micro-services/users/config/nodemailerConfig.js
+++ src/micro-services/users/config/nodemailerConfig.js
@@ -1,5 +1,5 @@
-const NODE_MAILER_USER = process.env.NODE_MAILER_USER || 'laughtolose@gmail.com';
-const NODE_MAILER_PASSWORD = process.env.NODE_MAILER_PASSWORD || 'Apple@develop123';
+const NODE_MAILER_USER = process.env.NODE_MAILER_USER || 'test@gmail.com';
+const NODE_MAILER_PASSWORD = process.env.NODE_MAILER_PASSWORD || 'gmailPassword';
 module.exports = {
   NODE_MAILER_USER,
   NODE_MAILER_PASSWORD,
